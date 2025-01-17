39dfccaca4982ea58354ef4ca75881953c3d8ea4
Pedro Paulo <101142218+pedroprmartins@users.noreply.github.com>
2025-01-17 19:35:49 -0300

    Corrigir o texto inicial segundário.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e ${numeroLimite}');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
 function verificarChute() {
